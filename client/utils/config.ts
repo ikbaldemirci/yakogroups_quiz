c1dc02bf982d898f4fdb675a37543459050988bc
ikbal <muhammed_5m@hotmail.com>
2026-03-29 18:30:15 +0300

    config fixes

--- client/utils/config.ts
+++ client/utils/config.ts
@@ -1,2 +1,3 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 export const CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_URL || "http://localhost:3000";
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
