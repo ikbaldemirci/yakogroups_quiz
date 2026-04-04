d36f1588ef1e27d40958e28ccdc72975a6dd7e73
ikbal <muhammed_5m@hotmail.com>
2026-04-04 16:58:54 +0300

    refactoring

--- server/index.js
+++ server/index.js
@@ -1,52 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// import express from "express";
-// import http from "http";
-// import { Server } from "socket.io";
-// import cors from "cors";
-
-// import connectDB from "./config/db.js";
-
-// import companyRoutes from "./routes/companyRoutes.js";
-// import quizRoutes from "./routes/quizRoutes.js";
-// import questionRoutes from "./routes/questionRoutes.js";
-// import gameSessionRoutes from "./routes/gameSessionRoutes.js";
-// import uploadRoutes from "./routes/uploadRoutes.js";
-// import authRoutes from "./routes/authRoutes.js";
-// import { gameSocket } from "./sockets/gameSocket.js";
-
-// const app = express();
-// const server = http.createServer(app);
-
-// await connectDB();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use("/uploads", express.static("uploads"));
-
-// app.use("/api/companies", companyRoutes);
-// app.use("/api/quizzes", quizRoutes);
-// app.use("/api/questions", questionRoutes);
-// app.use("/api/game-sessions", gameSessionRoutes);
-// app.use("/api/upload", uploadRoutes);
-// app.use("/api/auth", authRoutes);
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//   },
-// });
-
-// export { io };
-// gameSocket();
-
-// const PORT = process.env.PORT;
-
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -68,7 +19,13 @@ import { gameSocket } from "./sockets/gameSocket.js";
 const app = express();
 const server = http.createServer(app);
 
-await connectDB();
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+export { io };
 
 app.use(cors());
 app.use(express.json());
@@ -82,17 +39,6 @@ app.use("/api/game-sessions", gameSessionRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/auth", authRoutes);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*", 
-  },
-});
-
-export { io };
-gameSocket();
-
-const PORT = Number(process.env.PORT) || 3000;
-
 process.on("uncaughtException", (err) => {
   console.error("Uncaught Exception:", err);
 });
@@ -101,6 +47,14 @@ process.on("unhandledRejection", (reason, promise) => {
   console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = Number(process.env.PORT) || 5000;
+
+const startServer = async () => {
+  await connectDB();
+  gameSocket();
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
